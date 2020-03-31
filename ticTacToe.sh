f9ca8405eb93507164f19f97d0fd6bfff48c50cf
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-31 13:31:29 +0530

    ADDED: function to simulate toss

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -11,6 +11,21 @@ function resetBoard() {
 	done
 }
 
+function toss() {
+	t=$(($RANDOM%2))
+	if [ $t -eq 0 ]
+	then 
+		echo "You will play first"
+		players[0]="X"
+		players[1]="O"
+	else
+		echo "You will play second"
+		players[0]="O"
+		players[1]="X"
+	fi
+}
+
 echo "Welcome to Tic Tac Toe"
 resetBoard
 echo Your Letter is X
+toss
